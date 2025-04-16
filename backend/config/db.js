9116c2bc162bfe1f2ddab202eb3f7a9eebb690bf
Hiranshi Vekariya <123935854+Hiranshi157@users.noreply.github.com>
2025-04-16 10:39:47 +0530

    second commit

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB= async()=>{
-    await mongoose.connect("mongodb+srv://hiranshivekariya:ranjansorthiya1976@cluster0.3wf3n.mongodb.net/food-del").then(()=>console.log("DB Connected"));
-}
-
